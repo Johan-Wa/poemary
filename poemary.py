83bd6ebe2c0a29da2d54789368e81732dbe5e8ac
johan david <johandavidgp@gmail.com>
2022-12-19 12:36:33 -0500

    First functional vertion

--- poemary.py
+++ poemary.py
@@ -1,4 +1,4 @@
-# Imports
+#Imports
 from datetime import datetime
 from time import sleep
 import os 
@@ -6,25 +6,59 @@ import random
 
 # Variables
 path = '/home/dracul/programing/python/poemary/poems/'
-poems_list = [i.name for i in os.scandir(path)] 
+poems_list = [i.name for i in os.scandir(path)]
+
 # Funtions
-
 def select_poem():
-    '''Search in the poems dir, select a poem and charge it'''
-    pass
+    '''Search in the poems dir, select a poem and returns a dict with name of file and title'''
+    choice = random.choice(poems_list)
+    # To make the dir use choice as name_file and split the name file by the . and use the first item tu the tittle 
+    choice_info = {
+            'file_name': choice,
+            'title': choice.split('.')[0]
+            }
+    return choice_info
 
-def continue_poem():
+def load_poem(file_name):
+    '''Load de poem file'''
+    with open(path + file_name, 'r', encoding='utf-8') as f:
+        poem = f.readlines()
+    return poem
+
+def continue_poem(func):
     '''Clean the window and continue de poem after a pause'''
-    pass
+    def wrapper(*args, **kwargs):
+        os.system('clear')
+        print(f'                      {poem_info["title"]}              {datetime.now().strftime("%H:%M")}')
+        func(*args, **kwargs)
+        input('...')
+    return wrapper
 
-def declaim():
-    '''Declaim the choseen poem makinga pause in all the dots'''
-    pass
+@continue_poem
+def declaim(poem):
+    '''Declaim the choseen poem making a pause in each verse'''
+    global counter
+    for line in poem:
+        if line != poem[counter]:
+            continue
+        elif line == '\n':
+            break
+        else:
+            print(line)
+            sleep(1)
+            counter += 1
+        
 
 def run():
     '''Principal function that runs the script'''
-    print(poems_list)
-
+    global counter
+    global poem_info
+    counter = 0
+    poem_info = select_poem()
+    poem = load_poem(poem_info['file_name'])
+    while counter < len(poem):
+        declaim(poem)
+        counter += 1
 # Main
 
 if __name__ == '__main__':
